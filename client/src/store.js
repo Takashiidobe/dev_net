2606ee01022f391e62eebd7154b93e91a051ee13
takashiidobe <takashi.idobe@savvy.io>
2019-05-21 07:53:51 -0500

    fixed project for initialization

---
+++ client/src/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const initialState = {};
+
+const middleware = [thunk];
+
+//we need to provide compose from redux otherwise this won't work on browsers that don't use redux
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
+);
+
+export default store;
